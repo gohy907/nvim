0b36229c5573f849eef69c7b2537596eb820bde9
gohy <gohy2006@mail.ru>
2025-05-06 12:13:24 +0400

    fix(lspconfig.lua): have clangd work with non-ASCII chars

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -3,4 +3,17 @@ require("nvchad.configs.lspconfig").defaults()
 local servers = { "clangd" }
 vim.lsp.enable(servers)
 
+local base = require "nvchad.configs.lspconfig"
+local on_attach = base.on_attach
+local capabilities = base.capabilities
+
+local lspconfig = require "lspconfig"
+
+lspconfig.clangd.setup {
+  on_attach = function(client, bufnr)
+    client.server_capabilities.signatureHelpProvider = false
+    on_attach(client, bufnr)
+  end,
+  capabilities = capabilities,
+}
 -- read :h vim.lsp.config for changing options of lsp servers
