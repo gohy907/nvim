ded28dc30511c5593e599d1e2a372eaed2d76e02
gohy <gohy2006@mail.ru>
2025-05-06 14:43:42 +0400

    docs(chadrc.lua): write guidelines according to README.md

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -8,13 +8,18 @@ local M = {}
 M.base46 = {
   theme = "onedark",
 
+  -- If you don't want have comments to be italic, comment this part
+  --
   hl_override = {
     Comment = { italic = true },
     ["@comment"] = { italic = true },
   },
+  --
 }
 
+-- If you don't want have NvDash opening on start of Neovim change this 'true' to 'false'
 M.nvdash = { load_on_startup = true }
+
 M.ui = {
   tabufline = {
     lazyload = false,
