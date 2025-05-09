0690710887ef79e3db9c408373dea53813c0f15c
gohy <gohy2006@mail.ru>
2025-05-09 16:30:26 +0400

    fix(options.lua): fix number of spaces neovim is using on tabs to match clangd number

--- lua/options.lua
+++ lua/options.lua
@@ -2,5 +2,9 @@ require "nvchad.options"
 
 -- add yours here!
 
--- local o = vim.o
--- o.cursorlineopt = "number" -- to enable cursorline!
+local o = vim.o
+-- o.cursorlineopt = "both" -- to enable cursorline!
+o.tabstop = 4
+o.expandtab = true
+o.smartindent = true
+o.shiftwidth = 4
